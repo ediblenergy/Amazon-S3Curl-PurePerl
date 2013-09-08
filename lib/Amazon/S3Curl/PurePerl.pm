92cb8b5c29ea322bd26d1b38a036f40a47a2e816
skaufman <sam@socialflow.com>
2013-09-08 17:39:12 -0400

    got upload/download working

--- lib/Amazon/S3Curl/PurePerl.pm
+++ lib/Amazon/S3Curl/PurePerl.pm
@@ -2,7 +2,7 @@ package Amazon::S3Curl::PurePerl;
 use strict;
 use warnings FATAL => 'all';
 use Module::Runtime qw[ require_module ];
-
+use Devel::SimpleTrace;
 #ABSTRACT: Pure Perl s3 helper; generate params to pass to curl
 #For instances when you want to use s3, but don't want to install anything. ( and you have curl )
 use Moo;
@@ -14,6 +14,18 @@ use MIME::Base64 qw(encode_base64);
 use IPC::System::Simple qw[ capture ];
 use constant STAT_MODE => 2;
 use constant STAT_UID  => 4;
+our $DIGEST_HMAC;
+BEGIN {
+    eval {
+        require_module("Digest::HMAC");
+        $DIGEST_HMAC = "Digest::HMAC";
+    };
+    if ($@) {    #They dont have Digest::HMAC, use our packaged alternative
+        $DIGEST_HMAC = "Amazon::S3Curl::PurePerl::Digest::HMAC";
+        require_module($DIGEST_HMAC);
+    }
+};
+
 
 set_logger(
     Log::Contextual::SimpleLogger->new(
@@ -21,23 +33,6 @@ set_logger(
             levels_upto => 'debug'
         } ) );
 
-has digest_hmac => (
-    is      => 'ro',
-    builder => 1,
-);
-
-sub _build_digest_hmac {
-    my $digest_hmac;
-    eval {
-        require_module("Digest::HMAC");
-        $digest_hmac = "Digest::HMAC";
-    };
-    if ($@) {    #They dont have Digest::HMAC, use our packaged alternative
-        $digest_hmac = "Amazon::S3Curl::PurePerl::Digest::HMAC";
-        require_module($digest_hmac);
-    }
-    return $digest_hmac;
-}
 
 has curl => (
     is      => 'ro',
@@ -62,7 +57,6 @@ for (
 sub _req {
     my ( $self, $method ) = @_;
     $method ||= "GET";
-    warn $method;
     my $resource = $self->url;
     my $to_sign  = $self->url;
     $resource = "http://s3.amazonaws.com" . $resource;
@@ -71,12 +65,10 @@ sub _req {
     my $contentMD5  = "";
     my $contentType = "";
     my $xamzHeadersToSign = "";
-#    my $stringToSign = "$method\n$contentMD5\n$contentType\n$httpDate\n$xamzHeadersToSign$to_sign";
     my $stringToSign      = join( "\n" =>
           ( $method, $contentMD5, $contentType, $httpDate, "$xamzHeadersToSign$to_sign" ) );
-
     my $hmac =
-      $self->digest_hmac->new( $self->aws_secret_key, "Digest::SHA::PurePerl",
+      $DIGEST_HMAC->new( $self->aws_secret_key, "Digest::SHA::PurePerl",
         64 );
     $hmac->add($stringToSign);
     my $signature = encode_base64( $hmac->digest, "" );
@@ -106,7 +98,7 @@ sub upload {
     my $args = $self->_req('PUT');
     splice( @$args, $#$args, 0, "-T", $self->local_file );
     log_info { "running: " . join( " ", @_ ) } @$args;
-    warn capture(@$args);
+    capture(@$args);
     return $self->local_file;
 }
 
--- t/s3curl.t
+++ t/s3curl.t
@@ -3,8 +3,12 @@ use warnings FATAL => 'all';
 use Test::More;
 use Amazon::S3Curl::PurePerl;
 use File::Temp;
+use File::Spec;
+
 my $file = File::Temp->new;
 $file->autoflush(1);
+my $file2 = File::Temp->new;
+$file2->autoflush(1);
 die 'you need $ENV{AWS_ACCESS_KEY} and $ENV{AWS_SECRET_KEY}  and $ENV{S3_TEST_BUCKET} for the live tests.'
   unless $ENV{AWS_ACCESS_KEY} && $ENV{AWS_SECRET_KEY};
 ok my $pp = Amazon::S3Curl::PurePerl->new(
@@ -15,8 +19,17 @@ ok my $pp = Amazon::S3Curl::PurePerl->new(
   ),
   "Amazon::S3Curl::PurePerl instantiated";
 ok $pp->download;
-#my $test_string = "testo\ntesto2";
-#print $file $test_string;
+my $filename = (File::Spec->splitpath($file2))[-1];
+
+ok my $pp_uploader = Amazon::S3Curl::PurePerl->new(
+    aws_access_key => $ENV{AWS_ACCESS_KEY},
+    aws_secret_key => $ENV{AWS_SECRET_KEY},
+    local_file     => "$file",
+    url            => "/$ENV{S3_TEST_BUCKET}/$filename"
+);
+my $test_string = "testo\ntesto2";
+print $file2 $test_string;
+ok $pp_uploader->upload, "uploaded file";
 #ok $pp->upload;
 #ok my $pp = Amazon::S3Curl::PurePerl->new(
 #    aws_access_key => $ENV{AWS_ACCESS_KEY},
